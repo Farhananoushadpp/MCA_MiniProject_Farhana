4d2c8d2e7299ae956b0baa31634556e46f73fb2c
Farhananoushadpp <115775740+Farhananoushadpp@users.noreply.github.com>
2023-11-30 08:07:41 +0530

    Add files via upload

---
+++ lib/models/recipe_model.dart
@@ -0,0 +1,47 @@
+class RecipeModel {
+  final String? image;
+  final String? url;
+  final String? source;
+  final String? label;
+  final int? calories;
+  final String? nutritionalInformation;
+  final String? dietaryPrefs;
+  final String? ingredients;
+  final String? recipeDetails;
+  final String? mainIngredients;
+
+  RecipeModel({
+    this.image,
+    this.url,
+    this.label,
+    this.source,
+    this.calories,
+    this.nutritionalInformation,
+    this.dietaryPrefs,
+    this.ingredients,
+    this.recipeDetails,
+    this.mainIngredients,
+  });
+
+  factory RecipeModel.fromMap(Map<String, dynamic> parsedJson) {
+    return RecipeModel(
+        image: parsedJson["image"],
+        url: parsedJson["url"],
+        source: parsedJson["source"],
+        label: parsedJson["label"]);
+  }
+
+  factory RecipeModel.fromJSON(Map<String, dynamic> parsedJson) {
+    return RecipeModel(
+      image: parsedJson["image"],
+      label: parsedJson["recipeName"],
+      calories: parsedJson["calories"],
+      url: parsedJson["url"],
+      nutritionalInformation: parsedJson["nutritionalInformation"],
+      dietaryPrefs: parsedJson["dietaryPrefs"],
+      ingredients: parsedJson["ingredients"],
+      recipeDetails: parsedJson["recipeDetails"],
+      mainIngredients: parsedJson["mainIngredients"],
+    );
+  }
+}
